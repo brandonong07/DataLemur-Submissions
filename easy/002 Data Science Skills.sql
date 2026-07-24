ba38297e586c0275c1e0e6a21444170506ed0980
Brandon Ong <brandonong07@gmail.com>
2026-07-24 12:53:07 -0700

    Filled out template for Data Science SQL Problem

--- easy/002 Data Science Skills.sql
+++ easy/002 Data Science Skills.sql
@@ -1,15 +1,15 @@
 /*
-Platform: 
-Difficulty: 
-Topic: 
-Pattern:
+Platform: DataLemur
+Difficulty: Easy
+Topic: SQL
+Pattern: Aggregate Functions
 
-Problem Summary:
+Problem Summary: A company is looking for a Data Scientist and has a table of user_id(s) and their skills.
+Find user_id's where they fit all the skill counts
 
+Approach: First, find the amount of skills they satisfy then find user_id where skill count is 3
 
-Approach:
-
-Key Lesson:
+Key Lesson: Aggregate Functions can be used in "IF"-statements 
 
 */
 
